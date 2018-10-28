de13bcf2306c9da74516fff9a7c7549b7a403a2d
Viktor Jovanovic <vjovanov@e-r8-p6.s19.be>
2018-10-28 18:34:53 +0100

    modification strsplit

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -5,158 +5,106 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vjovanov <vjovanov@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/07 14:13:45 by vjovanov          #+#    #+#             */
-/*   Updated: 2018/10/27 15:22:50 by vjovanov         ###   ########.fr       */
+/*   Created: 2018/10/08 13:26:38 by bcedric           #+#    #+#             */
+/*   Updated: 2018/10/28 13:47:37 by vjovanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "libft.h"
 
-static size_t	count_words(char const *s, char c)
-{
-	size_t i;
-	size_t words;
-
-	i = 0;
-	words = 0;
-	if (s[0] != c && s[0] != '\0')
-		words++;
-	while (s[i])
-	{
-		if (s[i] == c)
-			if (s[i + 1] != c && s[i + 1] != '\0')
-				words++;
-		i++;
-	}
-	return (words);
-}
-
-static void		create_memwords(char const *s, char c, char **str)
+static size_t	ft_countletter(char const *s, char c)
 {
 	size_t	i;
-	size_t	j;
-	size_t	nb_letter;
+	size_t	countletter;
 
 	i = 0;
-	j = 0;
-	if (s[0] == c)
-		i = 1;
+	countletter = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
-			nb_letter++;
-		else
-		{
-			if (s[i - 1] != c)
-			{
-				str[j] = ft_strnew(nb_letter);
-				j++;
-				nb_letter = 0;
-			}
-		}
-		i++;
-	}
-	if (s[i - 1] != c && *s)
-		str[j] = ft_strnew(nb_letter);
-}
-
-static void		spliter(char const *s, char c, char **str)
-{
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	size_t	first;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	first = 0;
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			first = 1;
-			str[j][k] = s[i];
-			k++;
-		}
-		if (s[i] == c)
-			if (s[i + 1] != c && first == 1)
-			{
-				j++;
-				k = 0;
-			}
-		i++;
-	}
-}
-
-static int		check_malloc(char **str, size_t words)
-{
-	size_t i;
-
-	i = 0;
-	while (i < words)
-	{
-		if (str[i] == NULL)
+		if (s[i] != c && s[i + 1] != c && s[i + 1] != '\0')
+			countletter++;
+		else if (s[i] == c && s[i - 1] != c)
 			break ;
 		i++;
 	}
-	if (i != words)
-	{
-		i = 0;
-		while (i < words)
-		{
-			free(str[i]);
-			i++;
-		}
-		free(str[i]);
-		return (0);
-	}
-	return (1);
+	countletter++;
+	return (countletter);
 }
 
-/*
-** NAME:
-** 	ft_strsplit
-**
-** DESCRIPTION:
-**	ft_strsplit() alloue (avec malloc(3)) et retourne un tableau de chaines de
-**	caractères “fraiches” (toutes terminées par un ’\0’, le tableau également
-**	donc) résultant de la découpe de s selon le caractère c. Si l’allocation
-**	echoue, la fonction retourne NULL.
-**		Exemple :
-**		ft_strsplit("*salut*les***etudiants*", ’*’)
-**		renvoie le tableau ["salut", "les", "etudiants"].
-**
-** SYNOPSIS:
-**	char **ft_strsplit(char const *s, char c)
-**
-** PARAMS:
-**	s - la chaine de caractere qu'on veut splitter
-**	c - le caractere selon lequel decouper la chaine
-**
-** RETURN VALUE:
-**	(char**) Le tableau de chaines de caractères “fraiches” résultant de la
-**	découpe
-*/
+static size_t	ft_countwords(char const *s, char c)
+{
+	size_t	i;
+	size_t	countwords;
+
+	countwords = 0;
+	i = 0;
+	if (s[0] != c)
+	{
+		countwords++;
+		i++;
+	}
+	while (s[i])
+	{
+		if (s[i] != c && s[i - 1] == c)
+			countwords++;
+		i++;
+	}
+	return (countwords);
+}
+
+static char		*ft_word(char const *s, char c)
+{
+	char	*str;
+	size_t	i;
+	size_t	letter;
+
+	letter = ft_countletter(s, c);
+	str = ft_strnew(letter);
+	i = 0;
+	while (i < letter)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
+
+static void		*ft_remove(char **str, int index)
+{
+	while (index >= 0)
+	{
+		free(str[index]);
+		index--;
+	}
+	free(str);
+	return (NULL);
+}
 
 char			**ft_strsplit(char const *s, char c)
 {
+	size_t	i;
+	size_t	k;
 	char	**str;
-	size_t	words;
 
 	if (s == NULL)
 		return (NULL);
-	words = count_words(s, c);
-	str = (char**)malloc(sizeof(char*) * (words + 1));
+	i = 0;
+	k = 0;
+	str = (char**)malloc(sizeof(char*) * (ft_countwords(s, c)) + 1);
 	if (str == NULL)
 		return (NULL);
-	str[words] = 0;
-	create_memwords(s, c, str);
-	if (!(check_malloc(str, words)))
+	while (s[k])
 	{
-		free(str);
-		return (NULL);
+		if ((s[k] == c && s[k + 1] != c && s[k + 1] != '\0') ||
+			(k == 0 && s[k] != c))
+		{
+			str[i] = ft_word((k == 0 && s[k] != c) ? &s[k] : &s[k + 1], c);
+			if (str[i] == NULL)
+				return (ft_remove(str, i));
+			i++;
+		}
+		k++;
 	}
-	spliter(s, c, str);
+	str[i] = 0;
 	return (str);
 }
